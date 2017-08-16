43801504a97d2d28c35b47d1441545fef74057fc
Edwin Taquichiri <edwin.taquichiri@jalasoft.com>
2017-08-16 12:37:46 -0400

    added login feature

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -21,7 +21,7 @@
                 <ul class="dropdown-menu">
                     <li><a href="#">Profile</a></li>
                     <li class="divider"></li>
-                    <li><a href="login.html">Logout</a></li>
+                    <li><a href="<?=base_url().'auth/logout'?>">Logout</a></li>
                 </ul>
             </div>
  
